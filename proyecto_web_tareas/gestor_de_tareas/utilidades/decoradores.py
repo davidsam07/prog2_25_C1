67024a3d5494ec9734b51439da594955c0f3fa37
davidsam07 <jdsg7@alu.ua.es>
2025-04-10 23:51:04 +0200

    Revert "documentacion"
    This reverts commit 49940948103a3d63b7dc148c8c5d9d0f74ad58b1.

--- proyecto_web_tareas/gestor_de_tareas/utilidades/decoradores.py
+++ proyecto_web_tareas/gestor_de_tareas/utilidades/decoradores.py
@@ -1,20 +1,6 @@
 def log_funcion(funcion):
     """
-    Decorador para imprimir un log antes y después de la ejecución de una función.
-
-    Este decorador se utiliza para depurar el flujo de ejecución, mostrando en la consola
-    un mensaje cuando la función se inicia y cuando se termina de ejecutar.
-
-    Parameters
-    ----------
-    funcion : callable
-        La función que se desea decorar.
-
-    Returns
-    -------
-    callable
-        Una nueva función que envuelve la función original e imprime mensajes de log
-        antes y después de su ejecución.
+    Decorador para imprimir un log antes y después de llamar a la función.
     """
     def envoltura(*args, **kwargs):
         print(f"[LOG] Ejecutando: {funcion.__name__}")
